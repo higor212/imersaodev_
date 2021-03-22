d7f26c37b539a760847cd789ee83513d9c604ac2
Higor Lopes <higor2123456@gmail.com>
2021-03-22 17:55:18 -0300

    new file:   icon/temp.svg 	new file:   index.html 	new file:   script/index.js 	new file:   style/index.css

---
+++ script/index.js
@@ -0,0 +1,30 @@
+function conversor() {
+    // Valor que será digitado
+    let valorDigitado = document.querySelector("input#input-box").value
+
+    //Conversão do valorDigitado
+    let valorConvertido = parseFloat(valorDigitado)
+
+    //Valor da temperatura
+    const temperatura = 32
+
+    //Cálculo
+    let valorFinal = (valorConvertido - temperatura)/1.8
+    let valorDecimal = valorFinal.toFixed(2)
+
+    //Resultado
+    let mostrarValor = document.querySelector("div.write")
+    
+    mostrarValor.innerHTML = `A temperatura em celsius é: <strong>C°= ${valorDecimal}</strong>`
+    if(valorDigitado.length == 0 || valorDigitado <= 0) {
+        mostrarValor.innerHTML = "[ERRO] Por favor digite um número válido!"
+    }
+}
+
+/*Revisão 
+variáveis var int - float - string
+alert - parseInt - parseFloat - prompt
+operações + somar * multplicar
+comentário final
+toFixed
+*/
